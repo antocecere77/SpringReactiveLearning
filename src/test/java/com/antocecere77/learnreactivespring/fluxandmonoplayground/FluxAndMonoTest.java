33e8fbb0a2913af847cd3a0cb0155381b04c8e38
antocecere <antocecere77@gmail.com>
2020-08-02 11:56:16 +0200

    Flux - how it works ?

---
+++ src/test/java/com/antocecere77/learnreactivespring/fluxandmonoplayground/FluxAndMonoTest.java
@@ -0,0 +1,19 @@
+package com.antocecere77.learnreactivespring.fluxandmonoplayground;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+public class FluxAndMonoTest {
+
+    @Test
+    public void fluxTest() {
+        Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring")
+                //.concatWith(Flux.error(new RuntimeException("Exception occurred")))
+                .concatWith(Flux.just("After Error"))
+                .log();
+
+        stringFlux.subscribe(System.out::println,
+                (e) -> System.out.println("Exception is " + e),
+                () -> System.out.println("Completed"));
+    }
+}
