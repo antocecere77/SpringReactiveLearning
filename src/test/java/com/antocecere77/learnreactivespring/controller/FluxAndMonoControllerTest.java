7adb25d27a961b1f544326828266b508b40fadf0
antocecere <antocecere77@gmail.com>
2020-08-04 07:49:25 +0200

    Build the simple non blocking api - mono

--- src/test/java/com/antocecere77/learnreactivespring/controller/FluxAndMonoControllerTest.java
+++ src/test/java/com/antocecere77/learnreactivespring/controller/FluxAndMonoControllerTest.java
@@ -103,4 +103,18 @@ public class FluxAndMonoControllerTest {
                 .thenCancel()
                 .verify();
     }
+
+    @Test
+    public void mono() {
+
+        Integer expectedValue = new Integer(1);
+
+        webTestClient.get()
+                .uri("/mono")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Integer.class)
+                .consumeWith(response -> assertEquals(expectedValue, response.getResponseBody()));
+    }
 }
