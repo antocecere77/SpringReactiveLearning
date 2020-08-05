93ee0b0efb03dd3b2719d7c926703b3834b46745
antocecere <antocecere77@gmail.com>
2020-08-05 08:47:44 +0200

    Reactive mongo repository operations using junit - read - part2

--- src/test/java/com/antocecere77/learnreactivespring/repository/ItemReactiveRepositoryTest.java
+++ src/test/java/com/antocecere77/learnreactivespring/repository/ItemReactiveRepositoryTest.java
@@ -24,7 +24,8 @@ public class ItemReactiveRepositoryTest {
     List<Item> itemList = Arrays.asList(new Item(null, "Samsung TV", 400.0),
             new Item(null, "LG TV", 420.0),
             new Item(null, "Apple Watch", 299.99),
-            new Item(null, "Beats Headphones", 149.99));
+            new Item(null, "Beats Headphones", 149.99),
+            new Item("ABC", "Bose Headphones", 149.99));
 
     @Before
     public void setUp() {
@@ -39,7 +40,15 @@ public class ItemReactiveRepositoryTest {
     public void getAllItems() {
         StepVerifier.create(itemReactiveRepository.findAll()) //0
             .expectSubscription()
-            .expectNextCount(4)
+            .expectNextCount(5)
+            .verifyComplete();
+    }
+
+    @Test
+    public void getItemById() {
+        StepVerifier.create(itemReactiveRepository.findById("ABC"))
+            .expectSubscription()
+            .expectNextMatches(item -> item.getDescription().equals("Bose Headphones"))
             .verifyComplete();
     }
 }
