3346f802c6cec16654ee6be56218f12a6934520b
antonio <a.cecere.esterno@almaviva.it>
2020-08-05 17:22:24 +0200

    Create an item endpoint

--- src/test/java/com/antocecere77/learnreactivespring/controller/v1/ItemControllerTest.java
+++ src/test/java/com/antocecere77/learnreactivespring/controller/v1/ItemControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.Arrays;
@@ -108,6 +109,21 @@ public class ItemControllerTest {
                 .uri(ItemsConstants.ITEM_END_POINT_V1.concat("/{id}"), "DEF")
                 .exchange()
                 .expectStatus().isNotFound();
+    }
+
+    @Test
+    public void createItem() {
+        Item item = new Item(null, "Iphone X", 999.99);
+        webTestClient.post()
+                .uri(ItemsConstants.ITEM_END_POINT_V1)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .body(Mono.just(item), Item.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody()
+                .jsonPath("$.id").isNotEmpty()
+                .jsonPath("$.description").isEqualTo("Iphone X")
+                .jsonPath("$.price").isEqualTo(999.99);
     }
 
 }
