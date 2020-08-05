d488ea4befc6647fd5de1c684bc3b7734f581e02
antocecere <antocecere77@gmail.com>
2020-08-05 08:41:40 +0200

    Reactive mongo repository operations using junit - read - part1

---
+++ src/test/java/com/antocecere77/learnreactivespring/repository/ItemReactiveRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.antocecere77.learnreactivespring.repository;
+
+import com.antocecere77.learnreactivespring.document.Item;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+@DataMongoTest
+@RunWith(SpringRunner.class)
+public class ItemReactiveRepositoryTest {
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    List<Item> itemList = Arrays.asList(new Item(null, "Samsung TV", 400.0),
+            new Item(null, "LG TV", 420.0),
+            new Item(null, "Apple Watch", 299.99),
+            new Item(null, "Beats Headphones", 149.99));
+
+    @Before
+    public void setUp() {
+        itemReactiveRepository.deleteAll()
+                .thenMany(Flux.fromIterable(itemList))
+                .flatMap(itemReactiveRepository::save)
+                .doOnNext(item -> System.out.println("Inserted item is " + item))
+                .blockLast(); //Block until save is finished, if not inserted test1 start before data is inserted
+    }
+
+    @Test
+    public void getAllItems() {
+        StepVerifier.create(itemReactiveRepository.findAll()) //0
+            .expectSubscription()
+            .expectNextCount(4)
+            .verifyComplete();
+    }
+}
