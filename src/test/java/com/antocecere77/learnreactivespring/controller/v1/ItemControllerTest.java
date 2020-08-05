f0a822b33bd44a0c958deb9c3c69a287b02391be
antonio <a.cecere.esterno@almaviva.it>
2020-08-05 17:03:09 +0200

    Get all items - junit test using webtestclient - part 2

--- src/test/java/com/antocecere77/learnreactivespring/controller/v1/ItemControllerTest.java
+++ src/test/java/com/antocecere77/learnreactivespring/controller/v1/ItemControllerTest.java
@@ -15,9 +15,12 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.Assert.assertTrue;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -55,5 +58,37 @@ public class ItemControllerTest {
                 .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
                 .expectBodyList(Item.class)
                 .hasSize(4);
+    }
+
+    @Test
+    public void getAllItems_approch2() {
+        webTestClient.get()
+                .uri(ItemsConstants.ITEM_END_POINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Item.class)
+                .hasSize(4)
+                .consumeWith(response -> {
+                    List<Item> items = response.getResponseBody();
+                    items.forEach(item -> {
+                        assertTrue(item.getId() !=null);
+                    });
+                });
+    }
+
+    @Test
+    public void getAllItems_approch3() {
+        Flux<Item> itemFlux = webTestClient.get()
+                .uri(ItemsConstants.ITEM_END_POINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .returnResult(Item.class)
+                .getResponseBody();
+
+        StepVerifier.create(itemFlux.log("value from network: "))
+                .expectNextCount(4)
+                .verifyComplete();
     }
 }
