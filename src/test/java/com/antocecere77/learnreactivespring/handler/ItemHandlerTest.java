4f0f950c7e1cb61ae55ac781d1f806446ea58f67
antonio <a.cecere.esterno@almaviva.it>
2020-08-06 15:27:03 +0200

    Get all items - junit test using webtestclient

---
+++ src/test/java/com/antocecere77/learnreactivespring/handler/ItemHandlerTest.java
@@ -0,0 +1,59 @@
+package com.antocecere77.learnreactivespring.handler;
+
+import com.antocecere77.learnreactivespring.constants.ItemsConstants;
+import com.antocecere77.learnreactivespring.document.Item;
+import com.antocecere77.learnreactivespring.repository.ItemReactiveRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@DirtiesContext
+@AutoConfigureWebTestClient
+@ActiveProfiles("test")
+public class ItemHandlerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    List<Item> itemList = Arrays.asList(new Item(null, "Samsung TV", 400.0),
+            new Item(null, "LG TV", 420.0),
+            new Item(null, "Apple Watch", 299.99),
+            new Item("ABC", "Beats Headphones", 149.99));
+
+    @Before
+    public void setUp() {
+        itemReactiveRepository.deleteAll()
+                .thenMany(Flux.fromIterable(itemList))
+                .flatMap(itemReactiveRepository::save)
+                .doOnNext(item -> System.out.println("Inserted item is " + item))
+                .blockLast(); //Block until save is finished, if not inserted test1 start before data is inserted
+    }
+
+    @Test
+    public void getAllItems() {
+        webTestClient.get()
+                .uri(ItemsConstants.ITEM_FUNCTIONAL_END_POINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Item.class)
+                .hasSize(4);
+    }
+}
