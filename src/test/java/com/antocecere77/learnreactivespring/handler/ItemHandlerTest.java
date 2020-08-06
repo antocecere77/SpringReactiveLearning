6bed30a301f5e80d5fa8c6d8ec7c1657fe02eee4
antonio <a.cecere.esterno@almaviva.it>
2020-08-06 16:09:17 +0200

    Write the junit test case for delete item functional endpoint

--- src/test/java/com/antocecere77/learnreactivespring/handler/ItemHandlerTest.java
+++ src/test/java/com/antocecere77/learnreactivespring/handler/ItemHandlerTest.java
@@ -123,4 +123,15 @@ public class ItemHandlerTest {
                 .jsonPath("$.description").isEqualTo("Iphone X")
                 .jsonPath("$.price").isEqualTo("999.99");
     }
+
+    @Test
+    public void testDeleteOneItem() {
+
+        webTestClient.delete().uri(ItemsConstants.ITEM_FUNCTIONAL_END_POINT_V1.concat("/{id}"), "ABC")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Void.class);
+
+    }
 }
