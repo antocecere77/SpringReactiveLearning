1bb5a9dc8aaf43e5be1258879218204f9a6bd153
antocecere <antocecere77@gmail.com>
2020-08-07 08:14:29 +0200

    Handling exceptions in functional web - junit

--- src/test/java/com/antocecere77/learnreactivespring/handler/ItemHandlerTest.java
+++ src/test/java/com/antocecere77/learnreactivespring/handler/ItemHandlerTest.java
@@ -158,4 +158,14 @@ public class ItemHandlerTest {
                 .exchange()
                 .expectStatus().isNotFound();
     }
+
+    @Test
+    public void runTimeException() {
+        webTestClient.get()
+                .uri("/fun/runtimeException")
+                .exchange()
+                .expectStatus().is5xxServerError()
+                .expectBody()
+                .jsonPath("$.message", "RuntimException occurred");
+    }
 }
